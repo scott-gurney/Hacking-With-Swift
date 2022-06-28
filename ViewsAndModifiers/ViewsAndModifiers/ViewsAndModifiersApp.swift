4d2adc372ec6108ee9391cc340a200305052d0c7
SD449 <52426901+SD449@users.noreply.github.com>
2022-06-28 22:37:37 +0100

    ViewsAndModifiers project

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Scott Gurney on 28/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
