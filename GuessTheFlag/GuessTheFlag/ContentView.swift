311debfc58ab823b8e71bd769466e35352fdce54
SD449 <52426901+SD449@users.noreply.github.com>
2022-06-29 19:30:57 +0100

    challenge - FlagImage

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    var flags: [String]
+    var number: Int
+    
+    var body: some View {
+        Image(flags[number])
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+}
+
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -15,7 +27,7 @@ struct ContentView: View {
     @State private var endOfGame = false
     @State private var scoreTitle = ""
     @State private var score = 0
-    @State private var gameNumber = 0
+    @State private var gameNumber = 0    
     
     var body: some View {
         ZStack {
@@ -45,10 +57,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                            FlagImage(flags: countries, number: number)
                         }
                     }
                 }
