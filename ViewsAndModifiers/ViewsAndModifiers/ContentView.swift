b4387232319b891c934759e8303ce322ed44893b
SD449 <52426901+SD449@users.noreply.github.com>
2022-06-29 19:40:44 +0100

    challenge - ViewModifier and Extension

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,24 @@
 
 import SwiftUI
 
+struct BlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle.bold())
+            .foregroundStyle(.blue)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(BlueTitle())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .titleStyle()
     }
 }
 
