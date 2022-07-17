11ede8b1bc9e53b48ad1b2f7e5d627b29c362842
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-17 18:09:25 +0100

    deleting core data

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
                 .navigationTitle("Bookworm")
                 .toolbar {
@@ -46,11 +47,24 @@ struct ContentView: View {
                             Label("Add book", systemImage: "plus")
                         }
                     }
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                    }
                 }
                 .sheet(isPresented: $showingAddScreen) {
                     AddBookView()
                 }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet){
+        for offset in offsets {
+            let book = books[offset]
+            
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
