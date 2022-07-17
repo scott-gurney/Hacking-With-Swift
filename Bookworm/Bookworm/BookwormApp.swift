bd5fb62f5d162b54d9055581e77178200135e0c2
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-17 16:31:54 +0100

    create app

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Scott Gurney on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
