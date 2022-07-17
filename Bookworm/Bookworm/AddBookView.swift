41fc2660bef18ee2f1210d7383ebab03b04c1b0c
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-17 17:08:26 +0100

    Add Book View

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,74 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Scott Gurney on 17/07/2022.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var review = ""
+    @State private var genre = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    TextField("Review", text: $review)
+                    
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                } header: {
+                    Text("Write a review")
+                }
+                
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: moc)
+                        
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.genre = genre
+                        newBook.review = review
+                        newBook.rating = Int16(rating)
+                        
+                        try? moc.save()
+                        dismiss()
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
