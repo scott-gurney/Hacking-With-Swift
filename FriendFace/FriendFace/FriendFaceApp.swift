81ac3d1aee7068fea3c81e7df6f0caac9005bb2d
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-19 20:23:54 +0100

    create app

---
+++ FriendFace/FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Scott Gurney on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendFace/FriendFace/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  FriendFace
+//
+//  Created by Scott Gurney on 19/07/2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+}
