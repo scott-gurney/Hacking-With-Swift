6836f4573728492798596f0cb878d1514c1131d2
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-22 13:45:47 +0100

    fetch function

--- FriendFaceV2/FriendFaceV2/ContentView.swift
+++ FriendFaceV2/FriendFaceV2/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+    }
+    
+    func fetchUsers() async {
+        guard users.isEmpty else { return }
+        
+        do {
+            let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            users = try decoder.decode([User].self, from: data)
+        } catch {
+            print("Download failed")
+        }
     }
 }
 
