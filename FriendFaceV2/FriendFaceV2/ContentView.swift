5a5b9a81a4d2df5232031d07ecd98ddd4f3e57c5
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-22 14:26:01 +0100

    added function updateCache

--- FriendFaceV2/FriendFaceV2/ContentView.swift
+++ FriendFaceV2/FriendFaceV2/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var users = [User]()
+    @Environment(\.managedObjectContext) var moc
     
     var body: some View {
         NavigationView {
@@ -45,6 +46,32 @@ struct ContentView: View {
         } catch {
             print("Download failed")
         }
+    }
+    
+    func updateCache(with downloadedUsers: [User]) {
+        for user in downloadedUsers {
+            let cachedUser = CachedUser(context: moc)
+            
+            cachedUser.id = user.id
+            cachedUser.isActive = user.isActive
+            cachedUser.name = user.name
+            cachedUser.age = Int16(user.age)
+            cachedUser.company = user.company
+            cachedUser.email = user.email
+            cachedUser.address = user.address
+            cachedUser.about = user.about
+            cachedUser.registered = user.registered
+            cachedUser.tags = user.tags.joined(separator: ",")
+            
+            for friend in user.friends {
+                let cachedFriend = CachedFriend(context: moc)
+                cachedFriend.id = friend.id
+                cachedFriend.name = friend.name
+                cachedUser.addToFriends(cachedFriend)
+            }
+        }
+        
+        try? moc.save()
     }
 }
 
