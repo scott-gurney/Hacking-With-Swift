b17595df20057c2838512bebcda38f77a912b993
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-22 13:35:46 +0100

    create app

---
+++ FriendFaceV2/FriendFaceV2/FriendFaceV2App.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceV2App.swift
+//  FriendFaceV2
+//
+//  Created by Scott Gurney on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
