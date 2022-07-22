b50d4bc9176c74529c6e9048c2c643a832bc7edc
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-22 13:57:31 +0100

    added UserView

---
+++ FriendFaceV2/FriendFaceV2/UserView.swift
@@ -0,0 +1,41 @@
+//
+//  UserView.swift
+//  FriendFaceV2
+//
+//  Created by Scott Gurney on 22/07/2022.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    let user: User
+    
+    var body: some View {
+        List {
+            Section {
+                Text(user.about)
+                    .padding(.vertical)
+            } header: {
+                Text("About")
+            }
+            
+            Section {
+                Text("Address: \(user.address)")
+                Text("Email: \(user.email)")
+            } header: {
+                Text("Contact details")
+            }
+            
+            Section {
+                ForEach(user.friends) { friend in
+                    Text(friend.name)
+                }
+            } header: {
+                Text("Friends")
+            }
+        }
+        .listStyle(.grouped)
+        .navigationTitle(user.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
