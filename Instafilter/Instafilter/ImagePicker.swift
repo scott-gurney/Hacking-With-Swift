e953b6437c453a4d6a5e11f7292542e353c907a5
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-27 20:12:25 +0100

    added ImagePicker

---
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -0,0 +1,52 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Scott Gurney on 27/07/2022.
+//
+
+import Foundation
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let prodiver = results.first?.itemProvider else { return }
+            
+            if prodiver.canLoadObject(ofClass: UIImage.self) {
+                prodiver.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    }
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+}
