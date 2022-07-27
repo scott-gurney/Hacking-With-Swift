dcaceace100b204f27acd1ba24f20baf78dac6b9
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-27 20:16:18 +0100

    added ImageSaver

---
+++ Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,20 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Scott Gurney on 27/07/2022.
+//
+
+import Foundation
+import SwiftUI
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
+
