9fce37570423487ff23649bf2a6693d2fe9dacdb
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-27 19:49:09 +0100

    create app

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Scott Gurney on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
