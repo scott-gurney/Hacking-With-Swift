ae1252d62e176ce314e26a2d588ae42349a16948
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-27 20:26:47 +0100

    updated ContentView

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filerIntensity = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filerIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save", action: save)
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+    
+    func save() {
+        
     }
 }
 
