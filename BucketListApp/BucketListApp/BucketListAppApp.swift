5b4122e8d1a545870752abcb271fd2bf25e4a1f4
SD449 <52426901+SD449@users.noreply.github.com>
2022-07-30 15:14:52 +0100

    create app

---
+++ BucketListApp/BucketListApp/BucketListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListAppApp.swift
+//  BucketListApp
+//
+//  Created by Scott Gurney on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
