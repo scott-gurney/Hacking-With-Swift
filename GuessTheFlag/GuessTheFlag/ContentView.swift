4b492721bba2d2103f278bb94c3314c9a2f0937f
SD449 <52426901+SD449@users.noreply.github.com>
2022-08-02 15:48:39 +0100

    added accessibility

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -21,6 +21,21 @@ struct FlagImage: View {
 
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
@@ -45,6 +60,7 @@ struct ContentView: View {
                 Text("Guess the flag")
                     .font(.largeTitle.bold())
                     .foregroundColor(.white)
+                    .accessibilityHidden(true)
                 
                 VStack(spacing: 15) {
                     VStack {
@@ -54,7 +70,11 @@ struct ContentView: View {
                         
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
+                            
                     }
+                    .accessibilityElement()
+                    .accessibilityLabel("Tap the flag of \(countries[correctAnswer])")
+                    
                     ForEach(0..<3) { number in
                         Button {
                             withAnimation() {
@@ -63,6 +83,7 @@ struct ContentView: View {
                         } label: {
                             FlagImage(flags: countries, number: number)
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         .rotation3DEffect(.degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z:0))
                         .opacity(selectedFlag == -1 || selectedFlag == number ? 1.0 : 0.25)
                         .scaleEffect(selectedFlag == -1 || selectedFlag == number ? 1.0 : 0.25)
